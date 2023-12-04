dec3ff4328e7df196679fc785baabd32b8fab7c4
Tanvir Anjom Siddique <tanvir.anjom.siddique@gmail.com>
2023-12-05 00:53:13 +0600

    nodeJS server bug fixed & add favicon.ico
    Previously if *.js file is requested insted index.html file sent to browser-->error occured. Fixed this problem

--- create_server/nodeJsServer.js
+++ create_server/nodeJsServer.js
@@ -9,28 +9,56 @@ const port = 3000; // what port we are going to listening to for our server
 // --------------Create Server using http library (parameter: Request & Response)--------------------
 const server = http.createServer( function(req, res){
     // res.write("hello world!");
-    //status code: 200->everything ok, 
+    //status code: 200->everything ok,(request send to server is valid) 
     //content type--> parse sent file as html
 
     res.writeHead(200 , { 'Content-Type': 'text/html' });
-    fs.readFile('../index.html', function(error, data){
+    console.log(`requested url: http://localhost:3000${req.url}`);
+
+    var fileName="";
+    if(req.url=="/"){
+        fileName = '../index.html';
+    }
+    else{
+        fileName = `..${req.url}`;
+        // '..' + '/fileName' 
+        // reg.url.substring(1) --> string[1:last] (without 1st character '/' (index 0) 
+    }
+    fs.readFile(fileName, function(error, data){
         if(error){
             res.writeHead(404);//status:not found
             res.write('Error: File not found');
         }else{
-            res.write(data);//show index.html
+            res.write(data);//send index.html, fileName file
             res.end();
         }
     });
     
 });
+/*
+here,
+Homepage url: http://localhost:3000/
+request for index.html file
+
+while parsing index.html file Browser requests fowolling urls consecutively to show the home page.
+http://localhost:3000/01_console_variable.js
+http://localhost:3000/02_string_array.js
+http://localhost:3000/03_object_Literal_JSON_loop.js
+http://localhost:3000/04_Map_Filter_Foreach.js
+http://localhost:3000/05_function_simpler.js
+
+http://localhost:3000/favicon.ico 
+--> (this icon is shown in Title bar of a webpage)
+*/
 
 
+// Start service of a Severs Port 
 server.listen(port, function(error){
     if(error){
         console.log('Something Went wrong',error)
     }else{
         console.log('Server is listening on port ' + port)
+        console.log(`Homepage url: http://localhost:${port}/`);
     }
 });
 
